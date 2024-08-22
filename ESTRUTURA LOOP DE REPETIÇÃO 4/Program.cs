7d36f8fee57ff473328b334048a47fc176c00e3d
Guilherme Rodrigues <157857247+GuilhermeRodOliveira@users.noreply.github.com>
2024-08-22 18:02:34 -0300

    Adicionar arquivos de projeto.

---
+++ ESTRUTURA LOOP DE REPETIÇÃO 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ESTRUTURA_LOOP_DE_REPETIÇÃO_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //4 - Escreva um programa que calcule a soma dos números de 1 a 100 usando um loop for.
+
+            Console.WriteLine("Irei calcular a soma dos números de 1 a 100.");
+            int numero = 100;
+
+            for (int i = 1; i <= 100; i++) 
+            {
+                numero += i;
+            }
+
+            Console.WriteLine($"A soma dos números de 1 a 100 é: {numero}");
+        }
+    }
+}
